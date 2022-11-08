990058db1f74258404ec9db6503e9b1266914fb8
f4faysal <f4faysals@gmai.com.com>
2022-11-09 01:47:43 +0600

    add a mongodb

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-const express = require('express')
-const cors = require('cors');
-const app = express()
-const port = 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = 5000;
+
+const service = require('./data/service.json')
+
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${DATA_PASSWORD}@cluster0.0t7ovhi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 //medilware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+
+
+app.get("/", (req, res) => {
+  console.log(process.env.NAME_user);
+  res.send("Hello World!");
+});
+
+app.get('/service', (req, res) => {
+  res.send(service)
 })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
