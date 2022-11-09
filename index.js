1bbeed95586a81c3366f0157896c33633fc855c0
f4faysal <f4faysals@gmai.com.com>
2022-11-09 10:53:38 +0600

    mongodb install

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = 5000;
-
-const service = require('./data/service.json')
-
-
-
-const uri = `mongodb+srv://${process.env.USER_NAME}:${DATA_PASSWORD}@cluster0.0t7ovhi.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
+const port = process.env.PORT || 5000;
 
 //medilware
 app.use(cors());
 app.use(express.json());
 
+  
 
+//FagBwIWzyHNE2Mqz
+//justicia_server
 
+const uri =
+  "mongodb+srv://justicia_server:FagBwIWzyHNE2Mqz@cluster0.0t7ovhi.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const serviceCollection = client.db("justicialower").collection("services");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 
 app.get("/", (req, res) => {
-  console.log(process.env.NAME_user);
   res.send("Hello World!");
 });
 
-app.get('/service', (req, res) => {
-  res.send(service)
-})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
