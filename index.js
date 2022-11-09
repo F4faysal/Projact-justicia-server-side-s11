7e8fb066ae418a327bb801120664fff373583470
f4faysal <f4faysals@gmai.com.com>
2022-11-10 02:45:31 +0600

    id api creat

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion , ObjectId} = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -9,33 +9,58 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-  
-
-//FagBwIWzyHNE2Mqz
-//justicia_server
-
-const uri =
-  "mongodb+srv://justicia_server:FagBwIWzyHNE2Mqz@cluster0.0t7ovhi.mongodb.net/?retryWrites=true&w=majority";
+ /**=======================================
+              mongodb
+  ======================================*/
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0t7ovhi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+ /**=======================================
+                mongodb
+  ======================================*/
+
 async function run() {
   try {
     const serviceCollection = client.db("justicialower").collection("services");
-
+    /**=======================================
+                  Total service
+      =======================================*/
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     });
+    /**=======================================
+                    3 service api
+      =======================================*/
+    app.get("/service/3", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+
+    /**=======================================
+                unick id service api
+      =======================================*/
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = {_id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+
   } finally {
   }
 }
 run().catch((err) => console.error(err));
-
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
